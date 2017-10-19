a9a546eca53eeec5ab0bba8ddce521238a486cd7
Anthony Verghese <anthony.verghese@gmail.com>
2017-10-19 16:16:13 -0400

    Changes to util.py to fix json issue

--- aide_document/utils.py
+++ aide_document/utils.py
@@ -5,7 +5,7 @@ import json
 
 def read_json(json_key, json_string):
     parsed_json = json.loads(json_string)
-    if parsed_json[json_key].hasOwnProperty('value') and parsed_json[json_key].hasOwnProperty('units'):
+    if 'value' in parsed_json[json_key] and 'units' in parsed_json[json_key]:
         print('hello world')
     else:
         print('goodbye world')
