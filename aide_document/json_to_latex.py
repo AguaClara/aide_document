d5a78e9a6d5858d2595326dfdbe25f1a92d75ab5
Matan Presberg <mgp64@cornell.edu>
2017-10-19 14:58:15 -0500

    progress on parsing json to latex, issue with json file, getting decode error

--- aide_document/json_to_latex.py
+++ aide_document/json_to_latex.py
@@ -3,4 +3,10 @@ from utils import read_json
 
 
 
-read_json(
+file = open('json/data.json','r')
+
+json_string = file.read()
+
+file.close()
+
+read_json("max_coag_dose_conc",json_string)
--- aide_document/utils.py
+++ aide_document/utils.py
@@ -3,7 +3,9 @@ General purpose utility library..
 """
 import json
 
-def read_json(json_key, json_string)
+def read_json(json_key, json_string):
     parsed_json = json.loads(json_string)
-    if parsed_json[json_key].hasOwnProperty('value') and parsed_json[json_key].hasOwnProperty('units')
-        print 'hello world'
+    if parsed_json[json_key].hasOwnProperty('value') and parsed_json[json_key].hasOwnProperty('units'):
+        print('hello world')
+    else:
+        print('goodbye world')
