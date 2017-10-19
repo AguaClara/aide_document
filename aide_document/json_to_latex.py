cfec9e1a676dc441df56c5ea2a8ff9be13148a3f
Matan Presberg <mgp64@cornell.edu>
2017-10-19 14:33:25 -0500

    starting to parse json into latex on master

---
+++ aide_document/json_to_latex.py
@@ -0,0 +1,6 @@
+
+from utils import read_json
+
+
+
+read_json(
--- aide_document/utils.py
+++ aide_document/utils.py
@@ -1,69 +1,9 @@
 """
 General purpose utility library..
 """
+import json
 
-import math
-
-
-class Constants(object):
-    """
-    Constants.
-    """
-
-    class Earth(object):
-        """
-        Earth constants.
-        """
-        GRAVITY = 9.80665
-
-    class Mars(object):
-        """
-        Mars constants.
-        """
-        GRAVITY = 9
-
-
-def orbital_speed(planet, planatary_radius, altitude):
-    """
-    Calculate the orbital speed of an object.
-
-    v = R*sqrt(g/(R+h))
-
-    >>> from math import *
-    >>> round(orbital_speed(Constants.Earth, 600000, 70), 3)
-    2425.552
-    """
-    total_altitude = planatary_radius + altitude
-    speed = planatary_radius * \
-        math.sqrt(planet.GRAVITY / total_altitude)
-    return speed
-
-
-def circumference(radius):
-    """
-    Calculate the circumference of an object.
-
-    2*pi*r
-
-    >>> from math import *
-    >>> round(circumference(600000), 3)
-    3769911.184
-    """
-    distance = 2 * math.pi * radius
-
-    return distance
-
-
-def orbital_period(planet, planatary_radius, altitude):
-    """
-    Calculate the orbital period of an object.
-
-    d = v*t
-
-    >>> from math import *
-    >>> round(orbital_period(Constants.Earth, 600000, 70), 3)
-    1554.43
-    """
-    cir = circumference(planatary_radius + altitude)
-
-    return cir / orbital_speed(planet, planatary_radius, altitude)
+def read_json(json_key, json_string)
+    parsed_json = json.loads(json_string)
+    if parsed_json[json_key].hasOwnProperty('value') and parsed_json[json_key].hasOwnProperty('units')
+        print 'hello world'
