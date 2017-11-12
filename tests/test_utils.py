185c691b823842061101e64e4b26b931334612aa
Anthony Verghese <anthony.verghese@gmail.com>
2017-11-12 16:47:14 -0500

    Made some more tests for json_to_latex.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ aide_document tests.
 
 import unittest
 import io
-from aide_document.utils import read_json
+from utils import read_json
 
 
 class TestUtils(unittest.TestCase):
@@ -27,6 +27,10 @@ class TestUtils(unittest.TestCase):
         out = read_json("test_key_1",json_string)
         self.assertEqual(out, "5")
 
+        #Value should not have type string
+        with self.assertRaises(TypeError):
+          read_json("test_key_3",json_string)
+
     def test_read_json_w_units(self):
         try:
             in_file = open(self.test1,"r")
@@ -39,5 +43,12 @@ class TestUtils(unittest.TestCase):
         out = read_json("test_key_2", json_string)
         self.assertEqual(out, "10.00 mg/L")
 
+        #Should not have 3 fields associated with a variable
+        with self.assertRaises(TypeError):
+          read_json("test_key_4", json_string)
+        
+        #Should not have a string associated with the Value field
+        with self.assertRaises(TypeError):
+          read_json("test_key_5", json_string)
 if __name__ == '__main__':
     unittest.main()
