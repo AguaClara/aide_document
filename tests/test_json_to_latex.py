ca6a6d06a082bb10690efe624cce1996464af718
Matan Presberg <mgp64@cornell.edu>
2017-11-12 15:44:13 -0600

    starting testing for json_to_latex

--- tests/test_json_to_latex.py
+++ tests/test_json_to_latex.py
@@ -2,7 +2,21 @@
 
 import unittest
 import io
-
+from aide_document.json_to_latex import json_to_latex_header
 
 class TestJson2Latex(unittest.TestCase):
+    """
+    Test aide_document's json_to_latex file
+    """
 
+    def test_1(self):
+       data_file = 'aide_document/json/test2.json' 
+       header_file = 'aide_document/latex/test1.tex'
+       header_file_check = 'aide_document/latex/test1_check.tex'
+        
+       json_to_latex_header(data_file, header_file)
+
+       f1 = open(header_file, 'r')
+       f2 = open(header_file_check, 'r')
+
+       assertEqual(f1.read(), f2.read())
