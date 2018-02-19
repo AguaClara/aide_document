1fe8e820b4e3245886561895d19efdb876b46199
Oliver Leung <oal22@cornell.edu>
2018-02-19 15:23:38 -0500

    Add documentation for jinja2

--- jinja_yaml/jinja2_yaml_example.py
+++ jinja_yaml/jinja2_yaml_example.py
@@ -2,17 +2,18 @@ import os
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
+# Set up template environment from "templates" folder
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False,
     loader=FileSystemLoader(os.path.join(PATH, 'templates')),
     trim_blocks=False)
 
-
+# Render template from template environment and context
 def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
-
+# Pass through template file and parameters
 def create_index_html():
     fname = "output.md"
     context = yaml.load(file('input.yml'))
