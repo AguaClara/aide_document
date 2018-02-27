6f0214067fc3185d8564ecfed152f97e6f5eaf6d
Anneliese Lu <anneliese@dhcp-hol-4924.redrover.cornell.edu>
2018-02-27 14:11:55 -0500

    rename; make it a function

---
+++ aide_document/example.py
@@ -0,0 +1,5 @@
+import yml_to_md as doc
+
+# [render_documentation_template] takes three file names:
+# <input>, <output>, <template>
+doc.render_documentation_template("input.yml", "output.md", "example.md")
