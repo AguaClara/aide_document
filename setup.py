593aeed368762078ec7885a56afe40eb705b1136
Oliver Leung <oal22@cornell.edu>
2018-03-01 13:57:21 -0500

    Remove incorrect installation requirements for PyPI

--- setup.py
+++ setup.py
@@ -122,8 +122,6 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[ # Optional
-        'os',
-        'yaml',
         'aide_design',
         'jinja2'],
 
