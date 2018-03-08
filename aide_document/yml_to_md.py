bdac02ba8658b493284c2a0b0f79caa3086fec44
yilin-lu <yl668@cornell.edu>
2018-03-08 14:32:55 -0500

    update the path

--- aide_document/yml_to_md.py
+++ aide_document/yml_to_md.py
@@ -8,9 +8,8 @@ def render_template(template_filename, context):
     PATH = os.path.dirname(os.path.abspath(__file__))
     TEMPLATE_ENVIRONMENT = Environment(
         autoescape=False,
-        loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+        loader=FileSystemLoader(PATH),
         trim_blocks=False)
-
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 def render_documentation_template(input_name, output_name, template_name):
@@ -18,13 +17,13 @@ def render_documentation_template(input_name, output_name, template_name):
     [render_documentation_template] takes three file names: <input>, <output>, <template>
     """
     # Declare output file and parameters
-    context = yaml.load(open(input_name))
+    context = yaml.load(open(data/input_name))
 
     # Making each value parse-able
     for key,value in context.items():
         context[key] = value
 
     # Final render
-    with open(output_name, 'w') as f:
+    with open(data/output_name, 'w') as f:
         output = render_template(template_name, context)
         f.write(output)
