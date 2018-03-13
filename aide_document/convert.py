d57dd5ba0e749b27cd88f321b39fe7b813cfbbd0
Oliver Leung <oal22@cornell.edu>
2018-03-13 14:20:12 -0400

    Add docx to markdown conversion functionality

--- aide_document/convert.py
+++ aide_document/convert.py
@@ -44,3 +44,20 @@ def md_to_pdf(input_filename, output_filename):
     """
 
     os.system("pandoc " + input_filename + " -o " + output_filename + ".pdf" )
+
+def docx_to_md(input_filename, output_filename):
+    """
+    docx_to_md converts input_filename file to md,
+    and the name of the md file is output_filename
+
+    Parameters
+    ----------
+    input_filename : string
+    name of file to convert to pdf
+
+    output_filename : string
+    name of the pdf file
+
+    """
+
+    os.system("pandoc -s " + input_filename + " -t markdown -o " + output_filename + ".md" )
--- aide_document/example.py
+++ aide_document/example.py
@@ -3,3 +3,4 @@ import convert
 # [render_documentation_template] takes three file names:
 # <input>, <output>, <template>
 convert.yaml_to_md("input.yml", "output.md", "template.md")
+convert.docx_to_md("EtFlocSedFiSpanish.docx", "EtFlocSetFi Spanish.md")
