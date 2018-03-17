cb77d4496584c58394ee1656c1e9df597957d19f
Anneliese Lu <yl668@cornell.edu>
2018-03-17 14:40:20 -0400

    make translate a modular

--- aide_document/translate.py
+++ aide_document/translate.py
@@ -1,18 +1,14 @@
 import google_trans
 
-with open("data/output.md") as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content]
-text = []
-
-for sentence in content:
-    t = google_trans.trans(sentence, "en", "es")
-    text = text + [t.encode('utf-8')]
-
-print text
-
-with open("data/translated.md", "a") as f:
-    for l in text:
-        f.write(l)
-        f.write("\n")
+def translate(filename, src, tar, dest):
+    with open("data/" + filename) as f:
+        content = f.readlines()
+    content = [x.rstrip() for x in content]
+    text = []
+    for elt in content:
+        t = google_trans.api(elt, src, tar)
+        text = text + [t.encode('utf-8')]
+    with open("data/" + dest, "w") as f:
+        for l in text:
+            f.write(l)
+            f.write("\n")
