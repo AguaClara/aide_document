709e34a7b10fb215bf24a4f218707bdf10ddc345
OutofT1me <oliverleung2@gmail.com>
2018-03-20 21:22:10 -0400

    Fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aide_document',
-    version='0.1.71',
+    version='0.1.7',
     description='Provides templating engine for use within the AguaClara AIDE.',
     url='https://github.com/AguaClara/aide_document',
     author='Cornell University AguaClara',
