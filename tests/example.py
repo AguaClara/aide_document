9cb6e8b2df003c88aa04bda13aa2c38c4c30ffa3
OutofT1me <oliverleung2@gmail.com>
2018-04-22 19:27:28 -0400

    Reorganize data files

--- tests/example.py
+++ tests/example.py
@@ -2,13 +2,13 @@ from aide_document import combine, translate
 
 # [yaml_to_md] takes three file names:
 # <input>, <output>, <template>
-combine.render_document('template.md', "data/start/input.yml", "data/finish/output.md")
+combine.render_document('template.md', "data/input.yml", "data/output.md")
 
-# convert.docx_to_md("data/start/EtFlocSedFiEnglish.docx", "data/finish/EtFlocSedFiEnglish")
+# convert.docx_to_md("data/EtFlocSedFiEnglish.docx", "data/EtFlocSedFiEnglish")
 
 # [translate] takes four arguments:
 # <input>, <source_language>, <target_language>, <output>
 # Notice: the output file will be created.
 # If there is a file with the same name, it gets replaced.
-#translate.translate("data/finish/output.md", "en", "es", "data/finish/translated.md")
+#translate.translate("data/output.md", "en", "es", "data/translated.md")
 #translate.translate("output.md", "en", "hi", "translated2.md")
