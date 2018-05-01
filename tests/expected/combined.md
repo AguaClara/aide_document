155fff41087fa4fb0f62323a57d54d2ad502f243
Oliver Leung <oal22@cornell.edu>
2018-05-01 01:00:27 -0400

    Pytest for combine

--- tests/expected/combined.md
+++ tests/expected/combined.md
@@ -1,5 +1,5 @@
 ---
-title: data/output
+title: actual/combined
 ---
 # Hello, the circleDiam is 80 mm
 ## This is an example of the documentation generation.
--- tests/test_aide_document.py
+++ tests/test_aide_document.py
@@ -1,15 +1,16 @@
 from aide_document import combine, convert, translate
+from filecmp import cmp
 
 def test_combine():
-    combine.render_document('template.md', 'actual/input.yml', 'actual/combined.md')
-    assert open('actual/combined.md') == open('expected/combined.md')
+    combine.render_document('template.md', 'actual/input.yaml', 'actual/combined.md')
+    assert cmp('actual/combined.md', 'expected/combined.md')
 
-def test_convert():
-    convert.md_to_pdf('data/output.md', 'data/output')
-    convert.docx_to_md("data/EtFlocSedFiEnglish.docx", "data/EtFlocSedFiEnglish")
+#def test_convert():
+#    convert.md_to_pdf('data/output.md', 'data/output')
+#    convert.docx_to_md("data/EtFlocSedFiEnglish.docx", "data/EtFlocSedFiEnglish")
 
-def test_translate():
-    translate.translate("data/output.md", "en", "es", "data/translated.md")
-    translate.translate("data/output.md", "en", "hi", "data/translated2.md")
+#def test_translate():
+#    translate.translate("data/output.md", "en", "es", "data/translated.md")
+#    translate.translate("data/output.md", "en", "hi", "data/translated2.md")
 
-combine.render_document('template.md', 'actual/input.yml', 'actual/combined.md')
+#combine.render_document('template.md', 'actual/input.yaml', 'actual/combined.md')
